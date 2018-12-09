58ef0e5e9f4b416fe72153935a4356851d0f8493
Hugues BUREAU <huguesbureau@hotmail.fr>
2018-12-09 22:54:12 +0100

    style(project): configure jfx, openjdk and other dependencies to properly work with intellij

---
+++ src/main/java/fr/esigelec/snackio/Test.java
@@ -0,0 +1,19 @@
+package fr.esigelec.snackio;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import static javafx.application.Application.launch;
+
+public class Test extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        System.out.println("HELLO WORLD");
+    }
+
+}
