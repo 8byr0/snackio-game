5a4e903ab685b536c0c486ebe49c30256bd63dd6
Hugues BUREAU <huguesbureau@hotmail.fr>
2018-12-10 12:05:00 +0100

    style(logger): implement log4j and basis configuration to output to console

--- src/main/java/fr/esigelec/snackio/Test.java
+++ src/main/java/fr/esigelec/snackio/Test.java
@@ -1,29 +1,19 @@
 package fr.esigelec.snackio;
 
-import com.esotericsoftware.kryonet.Client;
-import com.esotericsoftware.kryonet.Connection;
-import com.esotericsoftware.kryonet.Listener;
-import com.esotericsoftware.kryonet.Server;
-import com.esotericsoftware.minlog.Log;
-import fr.esigelec.snackio.networking.NetworkManager;
 import fr.esigelec.snackio.ui.MenuController;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.Menu;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
-import java.util.logging.Level;
+import java.io.InputStream;
+import java.util.Map;
+import java.util.Properties;
 
 
 public class Test extends Application {
+    public static final Logger logger = LogManager.getLogger(Test.class);
 
     public static void main(String[] args) {
         launch(args);
@@ -31,10 +21,28 @@ public class Test extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        logger.info("TEST info");
+        logger.debug("TEST");
         // Launch the stuff
         MenuController controller = MenuController.getInstance(primaryStage);
 
         controller.openMenu(MenuController.Menus.MAIN_MENU);
+
+        Properties props = new Properties();
+        try {
+            final InputStream inStream = Test.class.getClassLoader().getResourceAsStream("log4j.properties");
+            if (inStream != null) {
+                props.load(inStream);
+            } else {
+                System.out.println("not on classpath");
+            }
+        } catch (IOException e) {
+            logger.error("Exception ", e);
+        }
+
+        for (Map.Entry<Object, Object> entry : props.entrySet()) {
+            System.out.println(entry);
+        }
     }
 
 }
