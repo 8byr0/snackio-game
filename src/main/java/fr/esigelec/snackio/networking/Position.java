d195b843d52b285e92f1581f1800f7a8c0722bf9
Hugues BUREAU <huguesbureau@hotmail.fr>
2018-12-15 19:01:44 +0100

    docs(game): update Position documentation

--- src/main/java/fr/esigelec/snackio/networking/Position.java
+++ src/main/java/fr/esigelec/snackio/networking/Position.java
@@ -1,19 +1,25 @@
 package fr.esigelec.snackio.networking;
 
+/**
+ * This class defines a Position by its coordinates
+ */
 public class Position {
     public float x;
     public float y;
 
+    /**
+     * Class constructor
+     */
     public Position() {
     }
 
+    /**
+     * Class constructor with coordinates as param
+     * @param x x
+     * @param y y
+     */
     public Position(float x, float y){
         this.x = x;
         this.y = y;
-    }
-
-    @Override
-    public String toString(){
-        return x + " - " + y;
     }
 }
