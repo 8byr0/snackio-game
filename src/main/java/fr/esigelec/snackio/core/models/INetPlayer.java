bc2382caa0f9cfc9d65b98cd97b527ec5254f0a7
Hugues BUREAU <huguesbureau@hotmail.fr>
2018-12-15 19:04:46 +0100

    docs(game): INetPlayer documentation update

--- src/main/java/fr/esigelec/snackio/core/models/INetPlayer.java
+++ src/main/java/fr/esigelec/snackio/core/models/INetPlayer.java
@@ -1,6 +1,5 @@
 package fr.esigelec.snackio.core.models;
 
-import fr.esigelec.snackio.core.models.Player;
 import fr.esigelec.snackio.game.character.motion.Direction;
 import fr.esigelec.snackio.networking.Position;
 
@@ -10,8 +9,20 @@ import fr.esigelec.snackio.networking.Position;
  * When a client is connected to the Server, it can execute any of these methods.
  */
 public interface INetPlayer {
+    /**
+     * Method called when a new player registers on the server
+     *
+     * @param localPlayer the player that just registered
+     */
     void registerPlayer (Player localPlayer);
 
+    /**
+     * Method to notify the server that the Player associated to this NetPlayer insatance has been updated
+     *
+     * @param id ID of the updated Player
+     * @param position new Position on the Game's map
+     * @param direction new Direction on the Game's map
+     */
     void updatePlayerMotion(int id, Position position, Direction direction);
 
 }
