89652d222aa8ccba69ad2fddf19434534d6bb90c
Xinyue SHEN <vertshen2014@gmail.com>
2018-12-16 17:12:40 +0100

    ServerConfigMenu

--- src/main/java/fr/esigelec/snackio/ui/ServerConfigMenu.java
+++ src/main/java/fr/esigelec/snackio/ui/ServerConfigMenu.java
@@ -1,4 +1,29 @@
 package fr.esigelec.snackio.ui;
 
-public class ServerConfigMenu {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ServerConfigMenu implements Initializable {
+    @FXML
+    private TextField roomName;
+
+    @FXML
+    private ChoiceBox map;
+
+    @FXML
+    private ChoiceBox Mode;
+
+    public ServerConfigMenu() {
+        roomName.getText();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
