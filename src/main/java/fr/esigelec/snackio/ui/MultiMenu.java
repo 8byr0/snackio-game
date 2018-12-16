c97035361e2abf480b92deaaf992c90a0617cd5e
Xinyue SHEN <vertshen2014@gmail.com>
2018-12-16 17:33:03 +0100

    ServerConfigMenu

--- src/main/java/fr/esigelec/snackio/ui/MultiMenu.java
+++ src/main/java/fr/esigelec/snackio/ui/MultiMenu.java
@@ -6,27 +6,35 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MultiMenu implements Initializable {
 
     @FXML
-    private Button openJoinMenuButton;
+    private AnchorPane multiAnchorPane;
 
     @FXML
-    private Button openHostMenuButton;
+    private Button joinRoom;
+
+    @FXML
+    private Button hostRoom;
 
     private Stage stage;
-
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        openJoinMenuButton.setOnAction(this::openJoinMenu);
-        openHostMenuButton.setOnAction(this::openHostMenu);
+        joinRoom.setOnAction(this::openMulMenu);
+        hostRoom.setOnAction(this::openServerConfig);
     }
 
 <<<<<<< HEAD
+<<<<<<< HEAD
     public void openJoinMenu(ActionEvent actionEvent) {
+=======
+    public void openMulMenu(ActionEvent actionEvent) {
+        //stage = (Stage) multiAnchorPane.getScene().getWindow();
+>>>>>>> ServerConfigMenu
         MenuController.getInstance(stage).openMenu(MenuController.Menus.MAIN_MENU);
 =======
     public void openMulMenu(ActionEvent actionEvent) {
@@ -36,7 +44,7 @@ public class MultiMenu implements Initializable {
 >>>>>>> ServerConfigMenu
     }
 
-    public void openHostMenu (ActionEvent actionEvent) {
+    public void openServerConfig (ActionEvent actionEvent) {
         MenuController.getInstance(stage).openMenu(MenuController.Menus.SERVER_CONFIG_MENU);
     }
 }
--- src/main/java/fr/esigelec/snackio/ui/Snippet.java
+++ src/main/java/fr/esigelec/snackio/ui/Snippet.java
@@ -1,18 +1,11 @@
 package fr.esigelec.snackio.ui;
-import com.esotericsoftware.minlog.Log;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.Parent;
-import javafx.fxml.FXMLLoader;
-import javafx.fxml.*;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 public class Snippet implements Initializable {
@@ -35,13 +28,18 @@ public class Snippet implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 <<<<<<< HEAD
+<<<<<<< HEAD
         backButton.setDisable(true);
 =======
         //homeButton.setDisable(true);
         backButton.setOnAction(this::backPrevious);
 >>>>>>> ServerConfigMenu
+=======
+        homeButton.setDisable(true);
+        //backButton.setOnAction(this::backPrevious);
+>>>>>>> ServerConfigMenu
         homeButton.setOnAction(this::backtoHome);
-        settingsButton.setOnAction(this::openSettingsView);
+        settingsButton.setOnAction(this::opensettingsView);
 
     }
     public void backtoHome(ActionEvent actionEvent) {
@@ -51,17 +49,7 @@ public class Snippet implements Initializable {
         System.out.println("previous page");
     }
 
-    public void openSettingsView(ActionEvent actionEvent) {
+    public void opensettingsView(ActionEvent actionEvent){
         System.out.println("settings");
-        try {
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/menus/controllerConfig.fxml"));
-            Parent root1 = (Parent) fxmlLoader.load();
-            Stage stage = new Stage();
-            stage.setTitle("Controller settings");
-            stage.setScene(new Scene(root1));
-            stage.showAndWait();
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
     }
 }
