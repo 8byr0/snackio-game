6392f6485f9f13a03c5ccf2ed2a03417fe1f8195
Xinyue SHEN <vertshen2014@gmail.com>
2019-01-15 11:30:04 +0100

    @FIX: config combobox afficher image ok

--- src/main/java/fr/esigelec/snackio/ui/ServerConfigMenu.java
+++ src/main/java/fr/esigelec/snackio/ui/ServerConfigMenu.java
@@ -1,18 +1,14 @@
 package fr.esigelec.snackio.ui;
 
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -31,7 +27,7 @@ public class ServerConfigMenu implements Initializable {
     private ComboBox map;
 
     @FXML
-    private ComboBox mode;
+    private ComboBox<String> mode;
 
     private Stage stage;
     private String quest;
@@ -46,16 +42,20 @@ public class ServerConfigMenu implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         Snippet.setPreviousLocation(MenuController.Menus.MULTI_MENU);
-        ObservableList<String> options = FXCollections.observableArrayList("option 1", "option 2" , "option 3");
-        map = new ComboBox();
+        ObservableList<String> options = FXCollections.observableArrayList();
+        int i = 1;
+        String img1 = "http://icons.iconarchive.com/icons/vincentburton/diaguita-ceramic-bowl/128/Diaguita-Ceramic-Bowl-"
+                + i + "-icon.png";
+        i = 2;
+        String img2 = "http://icons.iconarchive.com/icons/vincentburton/diaguita-ceramic-bowl/128/Diaguita-Ceramic-Bowl-"
+                + i + "-icon.png";
+        options.addAll(img1,img2);
+        map = new ComboBox(options);
         mode = new ComboBox(options);
-        map.getItems().addAll(
-                "jacob.smith@example.com",
-                "isabella.johnson@example.com",
-                "ethan.williams@example.com",
-                "emma.jones@example.com",
-                "michael.brown@example.com"
-        );
+        map.setCellFactory(c -> new StatusListCell());
+        //map.setButtonCell(new StatusListCell());
+        mode.setCellFactory(c -> new StatusListCell());
+        //mode.setButtonCell(new StatusListCell());
 
         grid.add(map, 1, 1);
         grid.add(mode, 1, 4);
@@ -66,4 +66,21 @@ public class ServerConfigMenu implements Initializable {
         //roomName.getText();
         MenuController.getInstance(stage).openMenu(MenuController.Menus.MULTI_MENU);
     }
+}
+
+class StatusListCell extends ListCell<String> {
+    protected void updateItem(String item, boolean empty){
+        super.updateItem(item, empty);
+        setGraphic(null);
+        setText(null);
+        if(item!=null){
+
+            ImageView imageView = new ImageView(new Image(item));
+            imageView.setFitWidth(40);
+            imageView.setFitHeight(40);
+            setGraphic(imageView);
+            setText("choix");
+        }
+    }
+
 }
