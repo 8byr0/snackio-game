cd75821ef9fefd3bdad42b49f3497646bce4fea6
ngouala japhet <ngouala.japhet@gmail.com>
2019-01-18 14:13:08 +0100

    create server ok

--- src/main/java/fr/esigelec/snackio/ui/ServerConfigMenu.java
+++ src/main/java/fr/esigelec/snackio/ui/ServerConfigMenu.java
@@ -3,6 +3,7 @@ import com.esotericsoftware.minlog.Log;
 import fr.esigelec.snackio.core.AbstractGameEngine;
 import fr.esigelec.snackio.core.NetworkGameEngine;
 import fr.esigelec.snackio.core.Player;
+import fr.esigelec.snackio.core.exceptions.GameCannotStartException;
 import fr.esigelec.snackio.core.exceptions.NoCharacterSetException;
 import fr.esigelec.snackio.core.exceptions.UnhandledCharacterTypeException;
 import fr.esigelec.snackio.core.exceptions.UnhandledControllerException;
@@ -10,11 +11,14 @@ import fr.esigelec.snackio.game.SnackioGame;
 import fr.esigelec.snackio.game.character.CharacterFactory;
 import fr.esigelec.snackio.game.map.MapFactory;
 import fr.esigelec.snackio.networking.client.SnackioNetClient;
+import fr.esigelec.snackio.networking.server.SnackioNetServer;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
+
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.URL;
 import java.util.*;
@@ -52,6 +56,8 @@ public class ServerConfigMenu implements Initializable {
             try {
                 try {
                     try {
+                        try{
+                            try{
                         SnackioGame game = SnackioGame.getInstance();
                         //        // Create the local player
                         Player myPlayer = new Player(playerName.getText(), CharacterFactory.CharacterType.valueOf(String.valueOf(character.getSelectionModel().getSelectedItem())));
@@ -62,9 +68,20 @@ public class ServerConfigMenu implements Initializable {
                         AbstractGameEngine engine = new NetworkGameEngine(game, myPlayer, MapFactory.MapType.valueOf(String.valueOf(map.getSelectionModel().getSelectedItem())));
                         //        // Instantiate a NetClient to exchange with client
                         SnackioNetClient cli = new SnackioNetClient(engine);
+                        NetworkGameEngine nEngine=new NetworkGameEngine(game,myPlayer,MapFactory.MapType.valueOf(String.valueOf(map.getSelectionModel().getSelectedItem())));
                         List<InetAddress> servers = cli.getAvailableServers();
-
+                        String[] difficultWords = new String[10];
+                        difficultWords[0]="you";
+                        SnackioNetServer.main(difficultWords);
+                        //cli.connectServer(servers.get(0));
+                        engine.startGame();
                         //000000000
+                        } catch (IOException e) {
+                            Log.error(e.getMessage(), e);
+                        }
+                        } catch (GameCannotStartException e) {
+                            Log.error(e.getMessage(), e);
+                        }
 
                     } catch (NoCharacterSetException e) {
                         Log.error(e.getMessage(), e);
