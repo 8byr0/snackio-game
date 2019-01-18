4dabc950071115b6470418c5121dc95711e2fcf3
ngouala japhet <ngouala.japhet@gmail.com>
2019-01-18 09:29:54 +0100

    everything ok excepted join and host(almost done)

--- src/main/java/fr/esigelec/snackio/ui/JoinRoomMenu.java
+++ src/main/java/fr/esigelec/snackio/ui/JoinRoomMenu.java
@@ -1,57 +1,128 @@
 package fr.esigelec.snackio.ui;
+
 import com.esotericsoftware.minlog.Log;
 import fr.esigelec.snackio.core.AbstractGameEngine;
 import fr.esigelec.snackio.core.NetworkGameEngine;
 import fr.esigelec.snackio.core.Player;
+import fr.esigelec.snackio.core.SoloGameEngine;
 import fr.esigelec.snackio.core.exceptions.GameCannotStartException;
 import fr.esigelec.snackio.core.exceptions.NoCharacterSetException;
 import fr.esigelec.snackio.core.exceptions.UnhandledCharacterTypeException;
 import fr.esigelec.snackio.core.exceptions.UnhandledControllerException;
 import fr.esigelec.snackio.game.SnackioGame;
 import fr.esigelec.snackio.game.character.CharacterFactory;
+import fr.esigelec.snackio.game.character.texture.AnimatedCharacterSkin;
 import fr.esigelec.snackio.game.map.MapFactory;
 import fr.esigelec.snackio.networking.client.SnackioNetClient;
-
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 import java.net.InetAddress;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
-public class JoinRoomMenu implements Initializable {
-    @FXML public ChoiceBox server_box;
-    @FXML public TextField testing;
+public class JoinRoomMenu  implements Initializable {
+    @FXML
+    private Button join;
+    @FXML
+    private TextField playerName;
+    @FXML
+    private Button refresh;
+    @FXML
+    private ComboBox character;
+
+    @FXML
+    private ChoiceBox server_box;
+    private Stage stage;
+
+
+    public JoinRoomMenu() {
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        Snippet.setPreviousLocation(MenuController.Menus.MULTI_MENU);
+        server("getInformation");
+        character.getItems().setAll(CharacterFactory.CharacterType.values());
+        refresh.setOnAction(this::refreshInfoServer);
+        join.setOnAction(this::connection);
 
+    }
+
+    public void connection(ActionEvent actionEvent) {
+        if(!playerName.getText().isEmpty() && !server_box.getSelectionModel().isEmpty() && !character.getSelectionModel().isEmpty()){
+            server("getConnection");
+        }
+    }
+
+    public void refreshInfoServer(ActionEvent actionEvent) {
+            server("getInformation");
+
+    }
+
+    public void server (String info) {
         try {
+            try {
                 try {
-                    try {
-                        //System.out.println(e.getAvailableServers());
-                        SnackioGame game = SnackioGame.getInstance();
-                //
-                //        // Create the local player
-                        Player myPlayer = new Player("Hugues", CharacterFactory.CharacterType.GOLDEN_KNIGHT);
-                //
-                //        /////////////// NETWORK CONTROL
-                //        // Instantiate Network game engine to control gameplay
-                        AbstractGameEngine engine = new NetworkGameEngine(game, myPlayer, MapFactory.MapType.DESERT_CASTLE);
-                //        // Instantiate a NetClient to exchange with client
-                        SnackioNetClient cli = new SnackioNetClient(engine);
-                        List<InetAddress> servers = cli.getAvailableServers();
+                    SnackioGame game = SnackioGame.getInstance();
+                    //        // Create the local player
+                    Player myPlayer = new Player("Hugues", CharacterFactory.CharacterType.GOLDEN_KNIGHT);
 
-                //        logger.debug(servers);
-                        if(servers.size() > 0) {
+                    if(info=="getConnection"){
+                        switch(String.valueOf(character.getSelectionModel().getSelectedItem())){
+                            case "INDIANA":
+                                myPlayer = new Player(playerName.getText(), CharacterFactory.CharacterType.INDIANA);
+                                break;
+                            case "INSPECTOR":
+                                myPlayer = new Player(playerName.getText(), CharacterFactory.CharacterType.INSPECTOR);;
+                                break;
+                            case "NUDE_MAN":
+                                myPlayer = new Player(playerName.getText(), CharacterFactory.CharacterType.NUDE_MAN);
+                                break;
+                            case "BALD_MAN":
+                                myPlayer = new Player(playerName.getText(), CharacterFactory.CharacterType.BALD_MAN);
+                                break;
+                            case "GOLDEN_KNIGHT":
+                                myPlayer = new Player(playerName.getText(), CharacterFactory.CharacterType.GOLDEN_KNIGHT);
+                                break;
+                        }
+
+                    }
+                    //
+                    //        /////////////// NETWORK CONTROL
+                    //        // Instantiate Network game engine to control gameplay
+                    AbstractGameEngine engine = new NetworkGameEngine(game, myPlayer, MapFactory.MapType.DESERT_CASTLE);
+                    //        // Instantiate a NetClient to exchange with client
+                    SnackioNetClient cli = new SnackioNetClient(engine);
+                    List<InetAddress> servers = cli.getAvailableServers();
+
+                    //        logger.debug(servers);
+                    if(servers.size() > 0) {
+                        System.out.println("servers>0");
+                        join.setDisable(false);
+                        if(info=="getConnection"){
+                            System.out.println("connection");
                             cli.connectServer(servers.get(0));
                         }
-                        System.out.println("youkhou"+servers.get(0));
-                    } catch (NoCharacterSetException e) {
-                        Log.error(e.getMessage(), e);
+                        if(info=="getInformation"){
+                            System.out.println("information");
+                            server_box.getItems().setAll(servers);
+                        }
+                    }else{
+                        join.setDisable(true);
+                        System.out.println("Aucun server");
+                        server_box.getItems().clear();
                     }
+                } catch (NoCharacterSetException e) {
+                    Log.error(e.getMessage(), e);
+                }
 
             } catch (UnhandledControllerException e) {
                 Log.error(e.getMessage(), e);
@@ -60,9 +131,5 @@ public class JoinRoomMenu implements Initializable {
                 UnhandledCharacterTypeException e) {
             Log.error(e.getMessage(), e);
         }
-
-
-    }
-    public static void info(List list){
     }
 }
--- src/main/java/fr/esigelec/snackio/ui/ServerConfigMenu.java
+++ src/main/java/fr/esigelec/snackio/ui/ServerConfigMenu.java
@@ -1,29 +1,28 @@
 package fr.esigelec.snackio.ui;
-
-import fr.esigelec.snackio.game.character.Character;
+import com.esotericsoftware.minlog.Log;
+import fr.esigelec.snackio.core.AbstractGameEngine;
+import fr.esigelec.snackio.core.NetworkGameEngine;
+import fr.esigelec.snackio.core.Player;
+import fr.esigelec.snackio.core.exceptions.NoCharacterSetException;
+import fr.esigelec.snackio.core.exceptions.UnhandledCharacterTypeException;
+import fr.esigelec.snackio.core.exceptions.UnhandledControllerException;
+import fr.esigelec.snackio.game.SnackioGame;
 import fr.esigelec.snackio.game.character.CharacterFactory;
-import fr.esigelec.snackio.game.character.texture.AnimatedCharacterSkin;
 import fr.esigelec.snackio.game.map.MapFactory;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+import fr.esigelec.snackio.networking.client.SnackioNetClient;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
-import javafx.stage.Stage;
-import org.lwjgl.system.CallbackI;
-
+import java.net.InetAddress;
 import java.net.URL;
 import java.util.*;
 
 public class ServerConfigMenu implements Initializable {
 
     @FXML
-    private TextField roomName;
+    private TextField playerName;
 
     @FXML
     private Button submit;
@@ -32,11 +31,7 @@ public class ServerConfigMenu implements Initializable {
     private ComboBox map;
 
     @FXML
-    private ComboBox mode;
-
-    private Stage stage;
-    private Scene scene;
-
+    private ComboBox character;
     @FXML
     private GridPane grid;
 
@@ -46,13 +41,43 @@ public class ServerConfigMenu implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         Snippet.setPreviousLocation(MenuController.Menus.MULTI_MENU);
+        List<Enum> somethingList = Arrays.asList(CharacterFactory.CharacterType.values());
         map.getItems().setAll(MapFactory.MapType.values());
-        mode.getItems().setAll(CharacterFactory.CharacterType.values());
+        character.getItems().setAll(CharacterFactory.CharacterType.values());
         submit.setOnAction(this::submitServer);
     }
 
     public void submitServer(ActionEvent actionEvent) {
-        MenuController.getInstance(stage).openMenu(MenuController.Menus.MULTI_MENU);
+        if(!map.getSelectionModel().isEmpty() && !character.getSelectionModel().isEmpty() && !playerName.getText().isEmpty()){
+            try {
+                try {
+                    try {
+                        SnackioGame game = SnackioGame.getInstance();
+                        //        // Create the local player
+                        Player myPlayer = new Player(playerName.getText(), CharacterFactory.CharacterType.valueOf(String.valueOf(character.getSelectionModel().getSelectedItem())));
+
+                        //
+                        //        /////////////// NETWORK CONTROL
+                        //        // Instantiate Network game engine to control gameplay
+                        AbstractGameEngine engine = new NetworkGameEngine(game, myPlayer, MapFactory.MapType.valueOf(String.valueOf(map.getSelectionModel().getSelectedItem())));
+                        //        // Instantiate a NetClient to exchange with client
+                        SnackioNetClient cli = new SnackioNetClient(engine);
+                        List<InetAddress> servers = cli.getAvailableServers();
+
+                        //000000000
+
+                    } catch (NoCharacterSetException e) {
+                        Log.error(e.getMessage(), e);
+                    }
+
+                } catch (UnhandledControllerException e) {
+                    Log.error(e.getMessage(), e);
+                }
+            } catch (
+                    UnhandledCharacterTypeException e) {
+                Log.error(e.getMessage(), e);
+            }
+        }
     }
 }
 
