f8366d031aa9edda4e1394f2daecce99f58dbb27
Hugues BUREAU <huguesbureau@hotmail.fr>
2019-02-05 17:05:19 +0100

    fix: rebase issues fixed

--- src/main/java/fr/esigelec/snackio/game/overlay/MapInformationOverlay.java
+++ src/main/java/fr/esigelec/snackio/game/overlay/MapInformationOverlay.java
@@ -33,12 +33,12 @@ public class MapInformationOverlay extends ApplicationAdapter {
     // PLAYER INFO
 
     // GAME INFO
-    AbstractGameState state;
+    private AbstractGameState state;
 
     // RENDERING
-    SpriteBatch batch;
-    BitmapFont font;
-    OrthographicCamera cam;
+    private SpriteBatch batch;
+    private BitmapFont font;
+    private OrthographicCamera cam;
     private ShapeRenderer shapeRenderer;
     private TextureRegion[] animationFrames;
     private Texture heart;
@@ -58,7 +58,7 @@ public class MapInformationOverlay extends ApplicationAdapter {
     @Override
     public void create() {
         heart = new Texture(Gdx.files.internal("poi/heart.png"));
-        heartRegion = new TextureRegion(heart,0,0,HEART_WIDTH,HEART_HEIGHT);
+        heartRegion = new TextureRegion(heart, 0, 0, HEART_WIDTH, HEART_HEIGHT);
         shapeRenderer = new ShapeRenderer();
 
         cam = new OrthographicCamera();
@@ -102,68 +102,59 @@ public class MapInformationOverlay extends ApplicationAdapter {
 
 
 
+        batch.setProjectionMatrix(cam.combined);
+        batch.begin();
+
+
         if (state instanceof CoinQuestGameState) {
-            shapeRenderer.setProjectionMatrix(cam.combined);
-            shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-            shapeRenderer.setColor(Color.RED);
-            shapeRenderer.rect(80, 80, 100, 10);
-            shapeRenderer.end();
-
-
-            batch.setProjectionMatrix(cam.combined);
-            batch.begin();
+//            shapeRenderer.setProjectionMatrix(cam.combined);
+//            shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+//            shapeRenderer.setColor(Color.RED);
+//            shapeRenderer.rect(80, 80, 100, 10);
+//            shapeRenderer.end();
+//
             batch.draw(getCurrentFrame(stateTime), 82, 85, COIN_WIDTH, COIN_HEIGHT);
 
             //show the coin information
             font.draw(batch, ((CoinQuestGameState) state).getFetchedCoins()
-                    + "/" + ((CoinQuestGameState) state).getCoinsToFetch()
+                            + "/" + ((CoinQuestGameState) state).getCoinsToFetch()
                     , 120, 115);
-            batch.end();
+
         }
 
-        if(state instanceof MultiplayerGameState){
-            batch.setProjectionMatrix(cam.combined);
-            batch.begin();
-            for (int n = 0; n < ((MultiplayerGameState) state).getActivePlayer().getLives(); n++){
-                batch.draw((TextureRegion) heartAnimation.getKeyFrame(stateTime, true), Gdx.graphics.getWidth()-160+n*40, 18,
+        if (state instanceof MultiplayerGameState) {
+            for (int n = 0; n < ((MultiplayerGameState) state).getActivePlayer().getLives(); n++) {
+                batch.draw((TextureRegion) heartAnimation.getKeyFrame(stateTime, true), Gdx.graphics.getWidth() - 160 + n * 40, 18,
                         HEART_WIDTH, HEART_HEIGHT);
             }
 
             //show the player information
             int offset = 1;
-            for(Player player : ((MultiplayerGameState) state).getPlayers()){
-                font.draw(batch, player.toString(), 200, 80+offset * 50);
+            for (Player player : ((MultiplayerGameState) state).getPlayers()) {
+                font.draw(batch, player.toString(), 200, 80 + offset * 50);
                 offset += 1;
-                for (int n = 0; n < player.getLives(); n++){
-                    batch.draw((TextureRegion) heartAnimation.getKeyFrame(stateTime, true), 350+n*40, offset * 50,
+                for (int n = 0; n < player.getLives(); n++) {
+                    batch.draw((TextureRegion) heartAnimation.getKeyFrame(stateTime, true), 350 + n * 40, offset * 50,
                             HEART_WIDTH, HEART_HEIGHT);
                 }
 
             }
             font.draw(batch, "Votre salle: " + ((MultiplayerGameState) state).getRoomName(),
-                    Gdx.graphics.getWidth()-450, Gdx.graphics.getHeight()-80);
-            font.draw(batch, "Vies restants: ", Gdx.graphics.getWidth()-400,50);
-            batch.end();
-        shapeRenderer.setProjectionMatrix(cam.combined);
-        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-        shapeRenderer.setColor(Color.YELLOW);
-        shapeRenderer.rect(80, 80, 100, 10);
-        shapeRenderer.end();
+                    Gdx.graphics.getWidth() - 450, Gdx.graphics.getHeight() - 80);
+            font.draw(batch, "Vies restants: ", Gdx.graphics.getWidth() - 400, 50);
+
+//            if (state.getGameMode() == GameMode.COINS_QUEST) {
+            batch.draw(texture, 70, Gdx.graphics.getHeight() - 85, HEART_WIDTH, HEART_HEIGHT);
+
+                font.draw(batch, " " + SnackioGame.getInstance().lives, 125, Gdx.graphics.getHeight() - 50);
+//            }
 
 
-        batch.setProjectionMatrix(cam.combined);
-        batch.begin();
-        if (state.getGameMode() == GameMode.COINS_QUEST) {
-            batch.draw(getCurrentFrame(stateTime), 82, 85, COIN_WIDTH, COIN_HEIGHT);
-            batch.draw(texture, 70, Gdx.graphics.getHeight()-85, HEART_WIDTH, HEART_HEIGHT);
-
-            font.draw(batch, ((CoinQuestGameState) state).getFetchedCoins() + "/" + ((CoinQuestGameState) state).getCoinsToFetch(), 120, 115);
-            font.draw(batch,"X " + SnackioGame.getInstance().lives,125,Gdx.graphics.getHeight()-50);
         }
-
-
+        batch.end();
 
     }
+
 
     private void renderCamera() {
 
@@ -184,7 +175,4 @@ public class MapInformationOverlay extends ApplicationAdapter {
         return (TextureRegion) coinAnimation.getKeyFrame(stateTime, true);
     }
 
-    private void ExtraInfoDisplay(){
-        //batch.draw();
-    }
 }
