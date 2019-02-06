bee147dc6ad7124ac164396a880ee37355c5b1f9
Hugues BUREAU <huguesbureau@hotmail.fr>
2019-02-06 05:24:31 +0100

    feat: allow client/server to be launched on a single computer

--- src/main/java/fr/esigelec/snackio/ui/ServerConfigMenu.java
+++ src/main/java/fr/esigelec/snackio/ui/ServerConfigMenu.java
@@ -4,9 +4,8 @@ import com.esotericsoftware.minlog.Log;
 import fr.esigelec.snackio.core.AbstractGameEngine;
 import fr.esigelec.snackio.core.NetworkGameEngine;
 import fr.esigelec.snackio.core.Player;
+import fr.esigelec.snackio.core.exceptions.GameCannotStartException;
 import fr.esigelec.snackio.game.SnackioGame;
-import fr.esigelec.snackio.game.character.CharacterFactory;
-import fr.esigelec.snackio.game.map.MapFactory;
 import fr.esigelec.snackio.networking.client.SnackioNetClient;
 import fr.esigelec.snackio.networking.server.SnackioNetServer;
 import fr.esigelec.snackio.ui.customButtons.AnimatedButton;
@@ -28,13 +27,12 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.URL;
 import java.util.*;
-import java.util.List;
 
 public class ServerConfigMenu implements Initializable {
 
     private static final int SQUARE_SIDE = 67;
     @FXML
-    private TextField playerName;
+    private TextField playerNameField;
 
     @FXML
     private TextField serverName;
@@ -76,24 +74,39 @@ public class ServerConfigMenu implements Initializable {
 
     private void createServer(ActionEvent actionEvent) {
         if (mapSelector.getSelectedToggle() != null && characterSelector.getSelectedToggle() != null
-                && !playerName.getText().isEmpty() && !serverName.getText().isEmpty()) {
+                && !playerNameField.getText().isEmpty() && !serverName.getText().isEmpty()) {
             try {
-                SnackioGame game = SnackioGame.getInstance();
-                // Create the local player
+                Player thePlayer = new Player(playerNameField.getText(), characterSelector.getSelectedCharacter());
 
-                Player myPlayer = new Player(playerName.getText(),
-                        characterSelector.getSelectedCharacter());
+
+                // Instantiate Network game engine to control gameplay
+                AbstractGameEngine engine = new NetworkGameEngine(SnackioGame.getInstance(), thePlayer);
+
+                // Instantiate a NetClient to exchange with client
+                SnackioNetClient cli = new SnackioNetClient();
+
+                cli.setOnConnectionSuccessfull((gameState) -> {
+                    try {
+                        engine.setGameState(gameState);
+                        engine.startGame();
+                    } catch (GameCannotStartException e) {
+                        e.printStackTrace();
+                    }
+                });
 
                 Thread serverThread = new Thread(()->{
                     try {
                         Log.set(Log.LEVEL_DEBUG);
                         SnackioNetServer srv = new SnackioNetServer(mapSelector.getSelectedMap(), serverName.getText());
                         srv.start();
+
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
                 });
                 serverThread.start();
+                cli.connectServer((InetAddress.getByName("127.0.0.1")), engine);
+
 
 //                engine.startGame();
 
